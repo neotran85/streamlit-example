e9a1c129e23bafa593aeb3146421ac0f8831d65f
neotran85 <namtran1985vn@gmail.com>
2023-10-04 13:34:27 +0000

    init code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,29 @@
+
+import streamlit as st
 from collections import namedtuple
 import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from sklearn.impute import SimpleImputer
+import seaborn as sns
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Load dataset
+data = pd.read_csv('campaign.csv', delimiter=';')
+print(data.columns)
+# Set default style
+sns.set_style("whitegrid")
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+plt.figure(figsize=(10, 6))
+sns.histplot(data['Income'].dropna(), kde=True, bins=30)
+plt.title('Income Distribution')
+plt.xlabel('Income')
+plt.ylabel('Number of Customers')
 
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Display the plot in Streamlit
+st.pyplot()
