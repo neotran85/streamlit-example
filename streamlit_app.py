e04562b7b1802b23f9273009dcae75aa8d9067e9
neotran85 <namtran1985vn@gmail.com>
2023-10-05 04:13:36 +0000

    Add Forecasted Number of Customers Using Prophet with Japanese Holidays

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,19 +11,127 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 from sklearn.impute import SimpleImputer
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from statsmodels.tsa.arima.model import ARIMA
+from prophet import Prophet
+
+
+# Hypothetical prior scales based on expected impact
+strong_effect = 10
+weak_effect = 0.5
+
+# Specifying the holidays with their expected effects
+holidays_data = {
+    'New Year\'s Day (Shōgatsu)': '2024-01-01', 
+    'Coming of Age Day (Seijin no Hi)': '2024-01-08',  # Might have moderate impact
+    'National Foundation Day (Kenkoku Kinen no Hi)': '2024-02-11', 
+    'The Emperor\'s Birthday': '2024-02-23', 
+    'Vernal Equinox Day (Shunbun no Hi)': '2024-03-20',
+    'Shōwa Day (Shōwa no Hi)': '2024-04-29',
+    'Constitution Memorial Day (Kenpō Kinenbi)': '2024-05-03',
+    'Greenery Day (Midori no Hi)': '2024-05-04',
+    'Children\'s Day (Kodomo no Hi)': '2024-05-05',
+    'Marine Day (Umi no Hi)': '2024-07-15',
+    'Mountain Day (Yama no Hi)': '2024-08-11', 
+    'Respect for the Aged Day (Keirō no Hi)': '2024-09-16',
+    'Autumnal Equinox Day (Shūbun no Hi)': '2024-09-22',
+    'Sports Day (Taiiku no Hi)': '2024-10-14',
+    'Culture Day (Bunka no Hi)': '2024-11-03', 
+    'Labor Thanksgiving Day (Kinrō Kansha no Hi)': '2024-11-23'
+}
+
+# Let's assume holidays like New Year's Day and Golden Week have a strong impact, while others have a weaker impact.
+strong_holidays = ['New Year\'s Day (Shōgatsu)', 'Shōwa Day (Shōwa no Hi)', 'Constitution Memorial Day (Kenpō Kinenbi)', 'Greenery Day (Midori no Hi)', 'Children\'s Day (Kodomo no Hi)']
+
+prior_scales = [strong_effect if holiday in strong_holidays else weak_effect for holiday in holidays_data.keys()]
+
+japanese_holidays = pd.DataFrame({
+    'holiday': list(holidays_data.keys()),
+    'ds': pd.to_datetime(list(holidays_data.values())),
+    'lower_window': 0,
+    'upper_window': 1,
+    'prior_scale': prior_scales
+})
+
+st.title("Forecasted Number of Customers Using Prophet with Japanese Holidays")
 
 # Load dataset
 data = pd.read_csv('campaign.csv', delimiter=';')
-print(data.columns)
-# Set default style
-sns.set_style("whitegrid")
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 
-plt.figure(figsize=(10, 6))
-sns.histplot(data['Income'].dropna(), kde=True, bins=30)
-plt.title('Income Distribution')
-plt.xlabel('Income')
-plt.ylabel('Number of Customers')
+# Convert 'Dt_Customer' column to datetime
+data['Dt_Customer'] = pd.to_datetime(data['Dt_Customer'])
 
-# Display the plot in Streamlit
-st.pyplot()
+# Resample the data by month and count the number of customers for each month
+customer_counts_monthly = data.resample('M', on='Dt_Customer').size()
+
+# Create a figure and ax object
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# Plot using ax
+customer_counts_monthly.plot(ax=ax)
+# Setting monthly labels
+ax.set_xticks(customer_counts_monthly.index)
+ax.set_xticklabels([months[i.month-1] for i in customer_counts_monthly.index], rotation=45)
+
+ax.set_ylabel('Number of Customers')
+ax.set_xlabel('Month')
+ax.set_title('Number of Customers Over Time (by Month)')
+ax.grid(True)
+st.pyplot(fig)
+
+
+
+# Resample data by month and count the number of customers for each month
+monthly_counts = data.resample('M', on='Dt_Customer').size().reset_index()
+monthly_counts.columns = ['ds', 'y']  # 'ds' for date and 'y' for value are required column names for Prophet
+
+# Initialize and fit the model
+model = Prophet(yearly_seasonality=True, holidays=japanese_holidays)
+model.fit(monthly_counts)
+
+# Create future dataframe for 2024 predictions
+future = model.make_future_dataframe(periods=12, freq='M')
+
+# Forecast
+forecast = model.predict(future)
+
+# Extract months and predicted y values for plotting
+forecasted_dates = forecast['ds'][-12:]  # Extracting only the last 12 months (i.e., the forecasted period for 2024)
+forecasted_values = forecast['yhat'][-12:]
+# Example: Replacing the outliers with the average of surrounding months
+forecasted_values.iloc[0] = forecasted_values.mean()
+forecasted_values.iloc[-1] = forecasted_values.mean()
+
+
+# Create a figure and ax object
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# Plot historical data
+ax.plot(monthly_counts['ds'], monthly_counts['y'], label='Historical', color='gray', linestyle='--')
+
+# Plot forecasted data
+ax.plot(forecasted_dates, forecasted_values, label='Forecasted', color='blue')
+
+# Setting monthly labels
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+ax.set_xticks(forecasted_dates)
+ax.set_xticklabels(months, rotation=45)
+
+ax.set_ylabel('Number of Customers')
+ax.set_xlabel('Month of 2024')
+ax.set_title('Forecasted Number of Customers Using Prophet')
+ax.grid(True)
+ax.legend()
+
+st.pyplot(fig)
+
+# Display Japanese holidays
+st.subheader("Japanese Holidays for 2024:")
+st.table(japanese_holidays.drop(columns=['lower_window', 'upper_window']))
+
+
